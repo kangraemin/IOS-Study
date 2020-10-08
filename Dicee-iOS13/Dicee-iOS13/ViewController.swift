4d1ec0bc225d76965205d66a1566c363538ebc62
kangraemin <remin1994@gmail.com>
2020-10-09 01:05:38 +0900

    Add logic to change dice imageView for button listener study

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Who         what    value
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+//        diceImageView1.alpha = 0.5
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+//        print("Button got tapped.")
+//        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+//        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][6 - leftDiceNumber]
+        
+        leftDiceNumber = leftDiceNumber + 1
+    }
+    
 }
 
