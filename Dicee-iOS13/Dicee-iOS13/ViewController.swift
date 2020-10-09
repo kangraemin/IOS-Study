55b83aa3e538cb4689d29e6e0ab880646fae95b0
kangraemin <remin1994@gmail.com>
2020-10-09 18:12:00 +0900

    End Dicee lecture - Use randomElement for select dice

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -14,26 +14,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
+    var leftDiceNumber = 0
+    var rightDiceNumber = 0
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Who         what    value
-//        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-//        diceImageView1.alpha = 0.5
-//        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Who         what    value
+////        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+////        diceImageView1.alpha = 0.5
+////        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+//
+//    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
 //        print("Button got tapped.")
 //        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
 //        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][6 - leftDiceNumber]
         
-        leftDiceNumber = leftDiceNumber + 1
+        // let vs var
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+//        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+//        leftDiceNumber += 1
+        
+//        leftDiceNumber = Int.random(in: 0...5)
+//        rightDiceNumber = Int.random(in: 0...5)
     }
     
 }
