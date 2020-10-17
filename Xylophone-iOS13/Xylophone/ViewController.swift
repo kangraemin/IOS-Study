5143643e872bc5f8221e64b24faad6c7a2ec044c
kangraemin <remin1994@gmail.com>
2020-10-17 14:47:43 +0900

    Add logic to play Xylophone sound using constructor

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -5,17 +5,66 @@
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    // IB -> Interface Builder
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        print(sender.titleLabel?.text)
+//        print(sender.currentTitle)
+        playSound(soundTitle: sender.currentTitle!)
+    }
     
-
+    func playSound(soundTitle: String) {
+        let url = Bundle.main.url(forResource: soundTitle, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
+//
+//import UIKit
+//import AVFoundation
+//
+//class ViewController: UIViewController {
+//
+//    var player: AVAudioPlayer?
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//    @IBAction func keyPressed(_ sender: UIButton) {
+////        print("I got pressed")
+//        playSound()
+//    }
+//
+//    func playSound() {
+//        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+//
+//        do {
+//            // Pink code -> swift code
+//            // Purple code -> Apple API code
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//
+//            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+//
+//            guard let player = player else { return }
+//
+//            player.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
+//}
+//
