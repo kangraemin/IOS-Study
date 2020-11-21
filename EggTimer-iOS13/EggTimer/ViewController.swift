c5f1780179b75499433b2543519a565633afb7f0
kangraemin <remin1994@gmail.com>
2020-11-21 19:44:32 +0900

    Add play sound when task completed

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -16,6 +17,7 @@ class ViewController: UIViewController {
     var totalCount = 0
     var count = 0
     var hardNess: String = ""
+    var player: AVAudioPlayer?
     
     @IBOutlet weak var remainingTimeLabel: UILabel!
     @IBOutlet weak var yorkProgressbar: UIProgressView!
@@ -50,9 +52,32 @@ class ViewController: UIViewController {
             remainingTimeLabel.text = hardNess + " Egg\n \(count) seconds remains"
             count -= 1
         } else if (count == 0) {
+            playSound()
             endTimer()
             yorkProgressbar.setProgress(1.0, animated: true)
             remainingTimeLabel.text = "done !"
+        }
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
         }
     }
 }
