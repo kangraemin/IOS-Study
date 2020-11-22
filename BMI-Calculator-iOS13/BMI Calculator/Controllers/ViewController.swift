52556c2e5744e642b22a44c309f70b895f1f3340
kangraemin <remin1994@gmail.com>
2020-11-22 21:18:30 +0900

    Add default height / weight slider

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSlider(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        weightLabel.text = String(Int(sender.value)) + "Kg"
+    }
 }
 
