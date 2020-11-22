ba57e019da388cf11b6fb03ee1ec794c728acd5c
kangraemin <remin1994@gmail.com>
2020-11-22 20:22:18 +0900

    Do homework for MVC design pattern

--- Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let selectedTitle = sender.currentTitle!
+        storyBrain.nextStory(selectedTitle: selectedTitle)
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getStoryChoice1(), for: UIControl.State.normal)
+        choice2Button.setTitle(storyBrain.getStoryChoice2(), for: UIControl.State.normal)
+    }
 }
 
