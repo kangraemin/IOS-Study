7e64dfaec90cde4b0c8a5d07f15bd82af783afdf
kangraemin <remin1994@gmail.com>
2020-11-22 22:38:58 +0900

    Use struct instead class to study of difference with class / struct

--- ClassessDemo/ClassessDemo/main.swift
+++ ClassessDemo/ClassessDemo/main.swift
@@ -6,13 +6,20 @@
 //let skeleton2 = Enemy()
 //let skeleton3 = Enemy()
 
-let skeleton = Enemy()
+var skeleton = Enemy(health: 100, attackStrength: 10)
+var skeleton2 = skeleton
+var skeleton3 = Enemy(health: 100, attackStrength: 10)
 
-let dragon = Dragon()
-dragon.wingSpan = 5
-dragon.attackStrength = 15
-dragon.talk(speech: "My teeth are swords !")
-dragon.move()
-dragon.attack()
+skeleton.takeDamage(amount: 10)
+print(skeleton.health)
+print(skeleton2.health)
+print(skeleton3.health)
+
+//let dragon = Dragon()
+//dragon.wingSpan = 5
+//dragon.attackStrength = 15
+//dragon.talk(speech: "My teeth are swords !")
+//dragon.move()
+//dragon.attack()
 
 
