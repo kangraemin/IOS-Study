a125d98e5c8c7630e86eb92922a8895a7917cb0c
kangraemin <remin1994@gmail.com>
2020-11-22 22:19:13 +0900

    Add default enemy / dragon class to study inheritance

--- ClassessDemo/ClassessDemo/main.swift
+++ ClassessDemo/ClassessDemo/main.swift
@@ -1,11 +1,18 @@
-//
-//  main.swift
-//  ClassessDemo
-//
-//  Created by 강래민 on 2020/11/22.
-//
 
-import Foundation
+//print(skeleton.health)
+//skeleton.move()
+//skeleton.attack()
+//
+//let skeleton2 = Enemy()
+//let skeleton3 = Enemy()
 
-print("Hello, World!")
+let skeleton = Enemy()
+
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.talk(speech: "My teeth are swords !")
+dragon.move()
+dragon.attack()
+
 
