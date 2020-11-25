f13d419dfed6f18044ba26375541713dfa876aa9
kangraemin <remin1994@gmail.com>
2020-11-25 23:06:53 +0900

    Add logic to perform networking request / response

--- Clima-iOS13/Clima/Models/WeatherManager.swift
+++ Clima-iOS13/Clima/Models/WeatherManager.swift
@@ -9,10 +9,35 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=d40b8dfc9355905ad25bc60eeb126b0c&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=d40b8dfc9355905ad25bc60eeb126b0c&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
         print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        // 1. Create URL
+        if let url = URL(string: urlString) {
+            // 2. Create URLSession
+            let session = URLSession(configuration: .default)
+            // 3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
