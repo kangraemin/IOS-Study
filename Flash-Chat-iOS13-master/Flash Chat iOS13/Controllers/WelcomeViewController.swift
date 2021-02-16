f73cb5e7f7e1f72ebbce7d2b6daebda5ef1bfd1c
kangraemin <remin1994@gmail.com>
2021-02-16 22:28:41 +0900

    Add adding text animation to splash UI using default loop

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,23 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+//        let fruitBasket = ["Apple", "Pear", "Orange"]
+//        for fruit in fruitBasket {
+//            print(fruit)
+//        }
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            print("-----")
+            print(charIndex)
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (Timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-    
-
 }
