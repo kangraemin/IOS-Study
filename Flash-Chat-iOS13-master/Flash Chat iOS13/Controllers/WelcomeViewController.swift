756c30af1b17f3d66a4e766cb834634f438b29da
kangraemin <remin1994@gmail.com>
2021-02-17 22:09:06 +0900

    Use CLTypingLabel for text animation

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,30 +7,33 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "⚡️FlashChat"
         
 //        let fruitBasket = ["Apple", "Pear", "Orange"]
 //        for fruit in fruitBasket {
 //            print(fruit)
 //        }
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            print("-----")
-            print(charIndex)
-            print(0.1 * charIndex)
-            print(letter)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (Timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            print("-----")
+//            print(charIndex)
+//            print(0.1 * charIndex)
+//            print(letter)
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (Timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
     }
 }
