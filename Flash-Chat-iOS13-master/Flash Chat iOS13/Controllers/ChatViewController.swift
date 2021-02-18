f17493597f042759010f9b3ba70058ddc5fa47f6
kangraemin <remin1994@gmail.com>
2021-02-19 00:45:04 +0900

    Add logics to save chat message using firestore

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import FirebaseAuth
+import Firebase
 
 class ChatViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    let db = Firestore.firestore()
     
     var messages: [Message] = [
         Message(sender: "1@2.com", body: "Hey !"),
@@ -32,6 +35,19 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName)
+                .addDocument(data:
+                                [K.FStore.senderField: messageSender,
+                                 K.FStore.bodyField: messageBody
+                                ]) { (error) in
+                    if let e = error {
+                        print("There was an issue saving data to firestore, \(e)")
+                    } else {
+                        print("Successfully saved data")
+                    }
+                }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -52,14 +68,14 @@ extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         cell.label?.text = messages[indexPath.row].body
-//        cell.textLabel?.text = messages[indexPath.row].body
+        //        cell.textLabel?.text = messages[indexPath.row].body
         return cell
     }
 }
 
 extension ChatViewController: UITableViewDelegate {
-//    // Table view cell 클릭 listener, storyboard에서 selection = None으로하면 클릭이 금지됨 
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(indexPath.row)
-//    }
+    //    // Table view cell 클릭 listener, storyboard에서 selection = None으로하면 클릭이 금지됨
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        print(indexPath.row)
+    //    }
 }
