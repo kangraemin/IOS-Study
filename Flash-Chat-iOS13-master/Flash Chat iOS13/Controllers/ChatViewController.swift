a7b2feebff48b7711f9adce70bf5d3c3a2c6f75e
kangraemin <remin1994@gmail.com>
2021-03-28 23:08:27 +0900

    Add logics for ui / ux improvements

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -52,6 +52,8 @@ class ChatViewController: UIViewController {
                             self.messages.append(newMessage)
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                             }
                         }
                     }
@@ -62,6 +64,7 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: UIButton) {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+
             db.collection(K.FStore.collectionName)
                 .addDocument(data:
                                 [K.FStore.senderField: messageSender,
@@ -71,6 +74,9 @@ class ChatViewController: UIViewController {
                     if let e = error {
                         print("There was an issue saving data to firestore, \(e)")
                     } else {
+                        DispatchQueue.main.async {
+                            self.messageTextfield.text = ""
+                        }
                         print("Successfully saved data")
                     }
                 }
@@ -93,9 +99,26 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label?.text = messages[indexPath.row].body
+        cell.label?.text = message.body
         //        cell.textLabel?.text = messages[indexPath.row].body
+        
+        // This is a message from the current user.
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.leftImageView.isHidden = true
+            cell.rightImageview.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        } else {
+            // This is a message from another sender
+            cell.leftImageView.isHidden = false
+            cell.rightImageview.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        
         return cell
     }
 }
